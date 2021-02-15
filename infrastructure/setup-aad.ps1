608ce4c83415c7030bb5969d2f0c7cba949debb8
Steve Pettifer <stephen.pettifer@guestline.com>
2021-02-15 21:03:00 +0000

    Removes permissions before re-adding

--- infrastructure/setup-aad.ps1
+++ infrastructure/setup-aad.ps1
@@ -15,4 +15,8 @@ az webapp auth update -g $resourceGroup -n $appName --enabled true --action Logi
 # --api 00000003-0000-0000-c000-000000000000 == MS Graph AAD application id
 # --api-permissions e1fe6dd8-ba31-4d61-89e7-88639da4683d == User.Read permission
 
+# Remove all permissions then re-add.
+
+az ad app permission delete --id $appId
+
 az ad app permission add --id $appId --api 00000003-0000-0000-c000-000000000000 --api-permissions e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope
